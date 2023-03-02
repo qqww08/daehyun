520503c4a05331517284acb940d36279aa1d016f
DaeHyun <qqwwee08@naver.com>
2023-03-03 04:28:26 +0900

    fix: 사용자 페이지에서 어드민 이외에 다른 권한 접근시 차단 하도록 수정

--- src/views/pages/UserPage/index.tsx
+++ src/views/pages/UserPage/index.tsx
@@ -1,12 +1,17 @@
 import type { ReactElement } from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
 import { ErrorAlertProvider } from "~/views/components";
 import GnbHeader from "~/views/components/Header/GnbHeader";
 import UserCreateSection from "~/views/pages/UserPage/components/UserCreateSection";
 import UserPageAdmin from "~/views/pages/UserPage/components/UserPageAdmin";
+import { roleSelectState } from "~/views/recoil";
 
 const UserPage = () => {
+  const roleSelectValue = useRecoilValue(roleSelectState);
+  const isAdmin = roleSelectValue === "admin";
+  if (!isAdmin) return <div>접근 불가능한 기능입니다.</div>;
   return (
     <ErrorAlertProvider>
       <MainContainer>
